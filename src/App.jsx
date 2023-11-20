4b2c9c258c5d078400702854f869c2ec448283f8
Moong <tmdals9154@gmail.com>
2023-11-20 16:02:22 +0900

    🎉 [Init] 프로젝트 초기 세팅
    1. 절대경로 설정
    2. index.html 설정
    3. .env.local 파일추가
    4. pockethost 생성
    5. pockethost 연결 테스트중

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Button from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>안녕하세요!</div>
+      <Button />
     </>
   );
 }
