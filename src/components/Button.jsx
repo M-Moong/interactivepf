45c44a87211096493b13f1d7da2267af27ea6153
Moong <tmdals9154@gmail.com>
2023-11-20 17:15:42 +0900

    🎉 [Init] 브라우저 자동실행 설정, PocketHost 연동테스트 성공

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,30 @@
 import PocketBase from 'pocketbase';
-import React, {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 const pb = new PocketBase('https://pfpf.pockethost.io');
 
-
 function Button() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function getProductsImage() {
+      try {
+        const readProduct = await pb.collection('test').getFullList();
+
+        setData(readProduct);
+        console.log(readProduct);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getProductsImage();
+  }, []);
 
   return (
     <>
       <button type="button">버튼</button>
+      <button type="button">{data.length}</button>
     </>
   );
 }
