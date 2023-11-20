585dd81ee53a793c7a235be54ea1bdedf8c93cf2
Moong <tmdals9154@gmail.com>
2023-11-20 17:39:36 +0900

    [Merge] eslintrc.js 삭제 후 eslint와 prettier 설정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,29 +4,29 @@ import { useEffect, useState } from 'react';
 const pb = new PocketBase('https://pfpf.pockethost.io');
 
 function Button() {
-  const [data, setData] = useState();
+    const [data, setData] = useState();
 
-  useEffect(() => {
-    async function getProductsImage() {
-      try {
-        const readProduct = await pb.collection('test').getFullList();
+    useEffect(() => {
+        async function getProductsImage() {
+            try {
+                const readProduct = await pb.collection('test').getFullList();
 
-        setData(readProduct);
-        console.log(readProduct);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+                setData(readProduct);
+                console.log(readProduct);
+            } catch (error) {
+                console.error(error);
+            }
+        }
 
-    getProductsImage();
-  }, []);
+        getProductsImage();
+    }, []);
 
-  return (
-    <>
-      <button type="button">버튼</button>
-      <button type="button">{data.length}</button>
-    </>
-  );
+    return (
+        <>
+            <button type='button'>버튼</button>
+            <button type='button'>{data.length}</button>
+        </>
+    );
 }
 
 export default Button;
